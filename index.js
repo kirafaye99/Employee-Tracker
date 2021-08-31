8a6db7606e53c90f82b21572463b2a2aacf00f5d
kirafaye99 <kirademarco99@gmail.com>
2021-08-30 22:26:31 -0700

    adding fuctions for view all

--- index.js
+++ index.js
@@ -19,4 +19,58 @@ const userOptions = () => {
             ]
         }
     ])
+    .then((selection) => {
+        switch(selection.options) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addADepartment();
+                break;
+            case 'Add A Role':
+                addARole();
+                break;
+            case 'Add an Employee':
+                addAnEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+const viewAllDepartments = () => {
+    connection.query('SELECT * FROM department', (error, input) => {
+        if (error) throw error;
+        console.table(input);
+        userOptions();
+    })
+}
+
+const viewAllRoles = () => {
+    connection.query('SELECT * FROM roles', (error, input) => {
+        if (error) throw error;
+        console.table(input);
+        userOptions;
+    })
+}
+
+const viewAllEmployees = () => {
+    connection.query('SELECT * FROM employee', (error, input) => {
+        if (error) throw error;
+        console.table(input);
+        userOptions;
+    })
 }
