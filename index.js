9614eef4270e2e7d91f0bba65e6661b7f585ef61
kirafaye99 <kirademarco99@gmail.com>
2021-08-30 17:24:05 -0700

    options and sql

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+const connection = require('./config/connection')
+
+const userOptions = () => {
+    inquirer.prompt([
+        {
+            name: 'options',
+            type: 'list',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add A Department',
+                'Add A Role',
+                'Add An Employee',
+                'Update An Employee Role',
+                'Exit'
+            ]
+        }
+    ])
+}
